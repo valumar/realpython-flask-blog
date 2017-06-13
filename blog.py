ab3ad46aff48e780895eeabaa05e57d9571002c9
Petr Antonov <antpeter@gmail.com>
2017-06-13 10:39:03 +0300

    Add logic

--- blog.py
+++ blog.py
@@ -1,6 +1,7 @@
 # blog.py - controller
 
 # imports
+from functools import wraps
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 import sqlite3
@@ -34,9 +35,42 @@ def login():
             return redirect(url_for('main'))
     return render_template('login.html', error=error), status_code
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first.')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html', posts=posts)
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('insert into posts (title, post) values (?, ?)',
+                        [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
+
 
 @app.route('/logout')
 def logout():
